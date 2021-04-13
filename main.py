5a12d255050ef12a0e58018d6dad38d35626dda1
akeyi2018 <akeyi2016@gmail.com>
2021-04-13 21:42:02 +0900

    add

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from datetime import datetime 
 
 st.title('Hello world')
 
 st.write('test')
 st.write('update')
 
+d = st.date_input("転職予定日", datetime.date(2021,6,1))
+st.write(d)
