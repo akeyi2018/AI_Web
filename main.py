bf41e2bc0e620da40740e24ed2eb2d11eda44d46
akeyi2018 <akeyi2016@gmail.com>
2021-04-13 22:53:18 +0900

    add

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import streamlit as st
-from datetime import datetime 
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title('Hello world')
+st.title('活性化関数の比較')
 
-st.write('test')
-st.write('update')
+def sigmoid(x):
+    return 1 /( 1 + np.exp(-x))
 
-d = st.date_input("転職予定日", datetime.date(2021,6,1))
+def swish(x):
+    return x*sigmoid(x)
+
+def relu(x):
+    return x*(x>0.0)
+
+x = np.arange(-5.0,5.0,0.1)
+y = sigmoid(x)
+y2 = swish(x)
+y3 = relu(x)
+
+fig, ax = plt.subplots()
+
+ax.plot(x, y, label='sigmoid') 
+ax.plot(x, y2, label='swish')
+ax.plot(x, y3, label="relu")
+ax.legend()
+st.pyplot(fig)
+
+d = st.date_input("こどもの日：", datetime.date(2021,6,1))
 st.write(d)
